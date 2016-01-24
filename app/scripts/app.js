55a667969478e6f81f71b268b230c1d4668722f1
Ivan Protsyk <vanilaice1995@gmail.com>
2016-01-24 14:38:19 +0200

    config ui-router instead ng-router for SPA

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,22 +1,61 @@
 'use strict';
-angular.module('confusionApp', ['ngRoute'])
-.config(function($routeProvider) {
-  $routeProvider
+angular.module('confusionApp', ['ui.router'])
+.config(function ($stateProvider, $urlRouterProvider) {
+  $stateProvider
 
-  .when('/contactus', {
-    templateUrl: 'views/contactus.html',
-    controller: 'ContactController'
+  .state('app', {
+    url: '/',
+    views: {
+      'header': {
+        templateUrl: 'views/header.html'
+      },
+      'content': {
+        template: '<h1>To be Completed</h1>',
+        controller: 'IndexController'
+      },
+      'footer': {
+        templateUrl: 'views/footer.html'
+      }
+    }
   })
 
-  .when('/menu', {
-    templateUrl: 'views/menu.html',
-    controller: 'MenuController'
+  .state('app.aboutus', {
+    url: 'aboutus',
+    views: {
+      'content@': {
+        templateUrl: 'views/aboutus.html',
+        controller: 'ContactController'
+      }
+    }
+  })
+  .state('app.contactus', {
+    url:'contactus',
+    views: {
+      'content@': {
+        templateUrl: 'views/contactus.html',
+        controller: 'ContactController'
+      }
+    }
   })
 
-  .when('/menu/:id', {
-    templateUrl: 'views/dishdetail.html',
-    controller: 'DishDetailController'
+  .state('app.menu', {
+    url: 'menu',
+    views: {
+      'content@': {
+        templateUrl: 'views/menu.html',
+        controller: 'MenuController'
+      }
+    }
   })
 
-  .otherwise('/contactus');
+  .state('app.dishdetails', {
+    url: 'menu/:id',
+    views: {
+      'content@': {
+        templateUrl: 'views/dishdetail.html',
+        controller: 'DishDetailController'
+      }
+    }
+  });
+  $urlRouterProvider.otherwise('/');
 });
