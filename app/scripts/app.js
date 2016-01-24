4d5174130a9f0de86af4db5364682f4bd7ce2a2b
Ivan Protsyk <vanilaice1995@gmail.com>
2016-01-24 13:26:41 +0200

    configure routes for SPA

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,2 +1,22 @@
 'use strict';
-angular.module('confusionApp', []);
+angular.module('confusionApp', ['ngRoute'])
+.config(function($routeProvider) {
+  $routeProvider
+
+  .when('/contactus', {
+    templateUrl: 'views/contactus.html',
+    controller: 'ContactController'
+  })
+
+  .when('/menu', {
+    templateUrl: 'views/menu.html',
+    controller: 'MenuController'
+  })
+
+  .when('/menu/:id', {
+    templateUrl: 'views/dishdetail.html',
+    controller: 'DishDetailController'
+  })
+
+  .otherwise('/contactus');
+});
