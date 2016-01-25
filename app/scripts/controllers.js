ce0d3dca967f7e2d886d4cc6ba8a62e1c7a3b7db
Ivan Protsyk <vanilaice1995@gmail.com>
2016-01-26 00:22:58 +0200

    config http service

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -5,7 +5,18 @@ angular.module('confusionApp')
   $scope.tab = 1;
   $scope.filtText = '';
   $scope.showDetails = false;
-  $scope.dishes = menuFactory.getDishes();
+  $scope.showMenu = false;
+  $scope.message = 'Loading ...';
+  $scope.dishes = [];
+  menuFactory.getDishes()
+  .then(
+    function(response) {
+      $scope.dishes = response.data;
+      $scope.showMenu = true;
+    },
+    function(response) {
+      $scope.message = 'Error: ' + response.status + ' ' + response.statusText;
+    });
 
   $scope.select = function(setTab) {
     $scope.tab = setTab;
@@ -29,8 +40,19 @@ angular.module('confusionApp')
 
 .controller('DishDetailController', ['$scope', '$stateParams', 'menuFactory', function($scope, $stateParams, menuFactory) {
 
-  var dish = menuFactory.getDish(parseInt($stateParams.id, 10));
-  $scope.dish = dish;
+  $scope.dish = {};
+  $scope.showDish = false;
+  $scope.message = 'Loading ...';
+  menuFactory.getDish(parseInt($stateParams.id, 10))
+  .then(
+    function(response) {
+      $scope.dish = response.data;
+      $scope.showDish = true;
+    },
+    function(response) {
+      $scope.message = 'Error: ' + response.status + ' ' + response.statusText;
+    });
+
 }])
 
 .controller('DishCommentController', ['$scope', function($scope) {
@@ -100,7 +122,19 @@ angular.module('confusionApp')
 }])
 
 .controller('IndexController', ['$scope', 'menuFactory', 'corporateFactory', function ($scope, menuFactory, corporateFactory) {
-  $scope.dish = menuFactory.getDish(0);
+  $scope.dish = {};
+  $scope.showDish = false;
+  $scope.message = 'Loading ...';
+  menuFactory.getDish(parseInt(0))
+  .then(
+    function(response) {
+      $scope.dish = response.data;
+      $scope.showDish = true;
+    },
+    function(response) {
+      $scope.message = 'Error: ' + response.status + ' ' + response.statusText;
+    });
+
   $scope.promotion = menuFactory.getPromotion(0);
   $scope.leader = corporateFactory.getLeader(3);
 }])
